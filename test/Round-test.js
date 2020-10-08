5d8541b6e2466a59c6755a5b7bc88f538cd7c264
Jordon Smith <jordon00smith@gmail.com>
2020-10-07 22:23:34 -0600

    Add Round.js and Round-test.js files

---
+++ test/Round-test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+
+describe('Round', function() {
+
+  it('should be a function', function() {
+    const round = new Round();
+      
+    expect(Deck).to.be.a('function');
+  });
+}); 
