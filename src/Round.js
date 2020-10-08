abcfe71e31c371d6dd7ffc948603752a7dc13d57
Jordon Smith <jordon00smith@gmail.com>
2020-10-08 00:01:32 -0600

    Update Round.js and Round-test.js

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,18 @@
+const Turn = require('../src/Turn');
+
 class Round {
     constructor(deck) {
         this.deck = deck.cards;
+        this.turns = 0;
+        this.incorrectGuesses = [];
+    }
+    returnCurrentCard() {
+        return this.deck[0];
+    }
+    takeTurn(guess) {
+        this.turns += 1;
+        const turn = new Turn(guess, this.deck[0]);
+        turn.giveFeedback();
     }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -8,8 +8,66 @@ const Round = require('../src/Round');
 describe('Round', function() {
 
   it('should be a function', function() {
-    const round = new Round();
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
       
-    expect(Deck).to.be.a('function');
+    expect(Round).to.be.a('function');
+  });
+    
+  it('should take in a deck as a parameter', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+      
+    expect(round.deck).to.deep.equal([card1, card2, card3]);
+  });
+    
+  it('should return the current card', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+      
+    round.returnCurrentCard();
+      
+    expect(round.returnCurrentCard()).to.deep.equal(card1);
+  });
+    
+  it('turns should have a default value of 0', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+      
+    expect(round.turns).to.deep.equal(0);
+  });
+    
+  it('should keep a list of incorrect guesses', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+      
+    expect(round.incorrectGuesses).to.deep.equal([]);
+  });
+    
+  it('should be able to take turns', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+      
+    round.takeTurn('sea otter');
+      
+    expect(round.takeTurn('sea otter')).to.deep.equal('Correct!');
   });
 }); 
